cafc0f31e43679d23725d8bc546ff1be48c78ecd
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2018-12-04 23:48:13 +0530

    ref #4 - base keys and selector files added

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const apiKey = '9f4c077563ba8bb8ae6c256128da6a18';
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
---
+++ src/js/view/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    searchFrom: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
+}
